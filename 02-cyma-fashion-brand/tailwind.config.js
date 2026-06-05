aee6b2e75ddfb3e85d3b869a22c4002e086afea6
Timilehin Orimisan <eiimmiit@gmail.com>
2026-06-05 03:29:22 +0100

    added the neccessary fonts

--- 02-cyma-fashion-brand/tailwind.config.js
+++ 02-cyma-fashion-brand/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          greendeep: "#0D3B2E",
+          greenlight: "#1E5A47",
+          golddeep: "#C6A04D",
+          goldlight: "#D8B76A",
+          cream: "#F8F6F1",
+          dark: "#111111",
+          white: "#FFFFFF",
+        }
+      },
+      fontFamily: {
+        clash: ["ClashDisplay", "sans-serif"],
+        clashmedium: ["ClashDisplayMedium", "sans-serif"],
+        clashsemibold: ["ClashDisplaySemiBold", "sans-serif"],
+        clashbold: ["ClashDisplayBold", "sans-serif"],
+        inter: ["Inter", "serif"],
+      },
+    },
   },
   plugins: [],
 }
